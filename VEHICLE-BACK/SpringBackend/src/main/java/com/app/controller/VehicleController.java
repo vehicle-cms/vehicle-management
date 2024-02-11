96f5011f6b5d009cc301a5ba60e0a04a4a9d6e4f
bigblockofice <bigblockofice2@gmail.com>
2024-02-11 19:03:34 +0530

    vehicle controller 1.1

--- VEHICLE-BACK/SpringBackend/src/main/java/com/app/controller/VehicleController.java
+++ VEHICLE-BACK/SpringBackend/src/main/java/com/app/controller/VehicleController.java
@@ -5,13 +5,17 @@ import java.util.List;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.dto.VehicleDTO;
+import com.app.entities.Vehicle;
 import com.app.service.VehicleService;
 
 @RestController
@@ -29,5 +33,17 @@ public class VehicleController {
 	@GetMapping("/{empId}")
 	public VehicleDTO getVehicleDetails(@PathVariable @NotNull Long vehicleId) {
 		return vehicleService.getVehicleDetails(vehicleId);
+	}
+	
+	public ResponseEntity<?> getAllVehiclesPaginated(
+			@RequestParam(defaultValue = "0", required = false) int pageNumber,
+			@RequestParam(defaultValue = "10", required = false) int pageSize){
+		
+		List<VehicleDTO> vehicleList = vehicleService
+						.getAllVehiclesPaginated(pageNumber, pageSize);
+		if(vehicleList.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
+		return ResponseEntity.ok(vehicleList);
 	}
 }
