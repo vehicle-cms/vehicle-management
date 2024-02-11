7f6225e12b412054b32b83fdeafe765f5d10f91a
Sanket Solanke <105629065+Sanket-Solanke@users.noreply.github.com>
2024-02-11 16:55:06 +0530

    Update CityDao.java

--- VEHICLE-BACK/SpringBackend/src/main/java/com/app/dao/CityDao.java
+++ VEHICLE-BACK/SpringBackend/src/main/java/com/app/dao/CityDao.java
@@ -7,12 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.entities.City;
 
-<<<<<<< HEAD
 public interface CityDao extends JpaRepository<City,Long> {
 	List<City>findAll();
 	Optional<City>findByPincode(String pincode);
-=======
-public interface CityDao extends JpaRepository<City, Long> {
-
->>>>>>> bc5320de08002d9f687f08063e17e3e7b6514046
 }
