855f1322fb5a29b6035b9a01b53e2de257399cf8
bigblockofice <bigblockofice2@gmail.com>
2024-02-11 15:49:06 +0530

    vehicle dao 1.0

--- VEHICLE-BACK/SpringBackend/src/main/java/com/app/dao/VehicleDao.java
+++ VEHICLE-BACK/SpringBackend/src/main/java/com/app/dao/VehicleDao.java
@@ -1,9 +1,16 @@
 package com.app.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.app.entities.FuelType;
 import com.app.entities.Vehicle;
 
 public interface VehicleDao extends JpaRepository<Vehicle,Long> {
-
+	Optional<Vehicle> findByVehicleNumber(String vehicleNumber);
+	Optional<Vehicle> findByRegistration(String registration);
+	List<Vehicle> findByStatus(boolean status);
+	List<Vehicle> findByFuelType(FuelType fuelType);
 }
