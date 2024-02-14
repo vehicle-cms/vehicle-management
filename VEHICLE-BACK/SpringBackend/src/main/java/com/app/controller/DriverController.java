a333bd68ddb853597d4d1bfb5ab49f23a7d7ba25
Abhishek Shinde <shindeab19@gmail.com>
2024-02-14 18:54:09 +0530

    controller chnges

--- VEHICLE-BACK/SpringBackend/src/main/java/com/app/controller/DriverController.java
+++ VEHICLE-BACK/SpringBackend/src/main/java/com/app/controller/DriverController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ import com.app.entities.Role;
 import com.app.entities.User;
 import com.app.service.UserService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+//import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 
 @RestController
--- VEHICLE-BACK/SpringBackend/src/main/java/com/app/controller/ManagerController.java
+++ VEHICLE-BACK/SpringBackend/src/main/java/com/app/controller/ManagerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ import com.app.entities.Role;
 import com.app.entities.User;
 import com.app.service.UserService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+//import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 
 @RestController
