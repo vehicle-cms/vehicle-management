35fbd34b3875ec6d785261b264e8757be8992c70
Abhishek Shinde <shindeab19@gmail.com>
2024-02-20 15:01:27 +0530

    hot fix

--- VEHICLE-BACK/SpringBackend/src/main/java/com/app/controller/DriverController.java
+++ VEHICLE-BACK/SpringBackend/src/main/java/com/app/controller/DriverController.java
@@ -1,9 +1,7 @@
 package com.app.controller;
 
 import java.util.List;
-
 import javax.validation.constraints.NotNull;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
-
 import com.app.dto.UserDTO;
 import com.app.entities.Role;
-
 import com.app.service.UserService;
-
-//import io.swagger.v3.oas.annotations.parameters.RequestBody;
-
 
 @RestController
 @RequestMapping("/user/driver")
