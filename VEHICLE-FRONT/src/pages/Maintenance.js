9a0eaa5f17eafacae90f933eb11ed98df25638bd
bigblockofice <bigblockofice2@gmail.com>
2024-02-21 09:49:53 +0530

    switching branch

--- VEHICLE-FRONT/src/pages/Maintenance.js
+++ VEHICLE-FRONT/src/pages/Maintenance.js
@@ -52,8 +52,8 @@ export default function Maintenance() {
   const [File, setFile] = useState(null);
   const object = useRef(null);
 
-  const [Email, setEmail] = useState();
-  const [PhoneNo, setPhoneNo] = useState();
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
   const [Password, setPassword] = useState();
   const [page, setPage] = useState(1);
   const [pageSize] = useState(10);
@@ -69,6 +69,96 @@ export default function Maintenance() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const handleClick = event => {
+    object.current.click();
+  };
 
+    const handleChange1 = event => {
+    setFile(event.target.files[0]);
+  };
 
+  //table columns
+  const columns = [
+    {
+      title: 'Start Date',
+      dataIndex: 'startDate',
+      key: 'startDate',
+      render: (text, record) => (
+        <Space size="middle">
+          <span>{record?.startDate} {record?.startDate}</span>
+        </Space>
+      ),
+    },
+    {
+      title: 'End Date',
+      dataIndex: 'endDate',
+      key: 'endDate',
+      render: (text, record) => (
+        <Space size="middle">
+          <span>{record?.endDate} {record?.endDate}</span>
+        </Space>
+      ),
+    },
+    {
+      title: 'Vehicle',
+      dataIndex: 'vehicle',
+      key: 'vehicle',
+      width: '10%',
+    },
+    {
+      title: 'Parts',
+      dataIndex: 'parts',
+      key: 'parts',
+      width: '10%'
+    },
+      
+    // {
+    //   title: 'Last Used',
+    //   dataIndex: 'lastUsedAppAt',
+    //   key: 'lastUsedAppAt',
+    //   render: (text, record) => (
+    //     <span>{moment(record?.lastUsedAppAt).format('DD/MMM/YYYY')}</span>
+    //   ),
+    // }
+    // ,
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <p
+            onClick={() => {
+              setIsModalVisible3(true);
+              // deleteAdminHandler(dispatch, record?.adminCode);
+            }}
+          >
+            <Icon icon="ant-design:delete-filled" width="20" />
+          </p>
+        </Space>
+      ),
+    },
+  ];
+
+    //method section starts
+    const handleOk = () => {
+      setIsModalVisible(false);
+    };
+  
+    const handleCancel = () => {
+      setIsModalVisible(false);
+    };
+  
+    const handleOk3 = () => {
+      setIsModalVisible3(false);
+    };
+  
+    const handleCancel3 = () => {
+      setIsModalVisible3(false);
+    };
+  
+    const handleChange = evt => {
+      const value = evt;
+      setName(value);
+    };
+    //method section ends
 }
